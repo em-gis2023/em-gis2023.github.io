60ece9eae426c3d17c6239e27dbff5dd6070d473
em-gis2023 <135779610+em-gis2023@users.noreply.github.com>
2023-10-24 19:14:35 +0800

    Update urls.py

--- common/urls.py
+++ common/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('committee', views.toCommittee_view),
     path('program', views.toProgram_view),
     path('submission', views.toSubmission_view),
-    path('program/2023', views.toProgram2023_view),
+    path('program-2023', views.toProgram2023_view),
 
 ]
 
